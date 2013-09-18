f7b2f49ef452359ac62e0b8f3135056c47097533
Mitchell Rosen <mitchellwrosen@gmail.com>
2013-09-18 15:45:56 -0700

    sloch reworked to use pipes, generified a little bit for git support

--- Main.hs
+++ Main.hs
@@ -2,11 +2,15 @@ module Main where
 
 import System.Environment (getArgs)
 
-import Options (parseOptions)
-import Sloch (sloch)
+import Options (Options(..), parseOptions)
+import Sloch (slochHierarchy)
 
 main :: IO ()
 main = do
     (opts, [dir]) <- parseOptions =<< getArgs
-    s <- sloch dir opts
+
+    let depth            = optDepth opts
+        include_dotfiles = optIncludeDotfiles opts
+
+    s <- slochHierarchy dir depth include_dotfiles
     print s
--- Sloch.hs
+++ Sloch.hs
@@ -1,52 +1,77 @@
 {-# LANGUAGE LambdaCase, RankNTypes #-}
 
-module Sloch (Sloch, sloch) where
+module Sloch
+    ( Sloch
+    , SlochHierarchy
+    , slochHierarchy
+    , slochFiles
+    ) where
 
 import Control.Applicative ((<$>))
-import Control.Monad (unless)
-import Control.Monad.Trans (lift)
-import Control.Monad.Trans.State (StateT, execStateT, modify, put)
+import Control.Monad (forever, unless)
+import Control.Monad.Morph (hoist)
+import Control.Monad.Trans (lift, liftIO)
+import Control.Monad.Trans.State.Strict (StateT, execStateT, modify, put)
 import Control.Monad.Trans.Reader (ReaderT)
 import Data.Map (Map)
+import Pipes (Consumer', Effect, Pipe, Producer', (>->), await, each, runEffect, yield)
+import Pipes.Lift (execStateP)
 
 import qualified Data.Map      as M
+import qualified Pipes.Prelude as P
 
 import DirectoryTree (Dirent(..), DirectoryTree, makeTree, treesAtDepth)
 import Language (Language, language)
 import LineCounter (countLines)
 import Options (Options(..))
 
+-- A simple summary of the source-lines-of-code count, by language.
+type Sloch = Map Language Int
+
 -- A summary of the source-lines-of-code count, represented as two maps: the outer, a directory name to counts, and the
 -- inner, a map from language type to number of lines.
-type Sloch = Map FilePath (Map Language Int)
+type SlochHierarchy = Map FilePath Sloch
 
-sloch :: FilePath -> Options -> IO Sloch
-sloch path opts = do
-    let depth            = optDepth opts
-        include_dotfiles = optIncludeDotfiles opts
+slochHierarchy :: FilePath -> Int -> Bool -> IO SlochHierarchy
+slochHierarchy path depth include_dotfiles = do
     trees <- treesAtDepth depth <$> makeTree path include_dotfiles
-    execStateT (mapM_ sloch' trees) M.empty
+    execStateEffect M.empty $
+        hoist lift (each trees) >-> slochHierarchy'
+
+execStateEffect :: Monad m => s -> Effect (StateT s m) () -> m s
+execStateEffect init_state effect = runEffect $ execStateP init_state $ effect
 
 -- | "Outer" lines count, which builds up a mapping from directory name -> inner lines count. Only adds an entry if
 -- there were any lines counted.
-sloch' :: DirectoryTree -> StateT Sloch IO ()
-sloch' (path, children) = do
-    lang_to_count_map <- lift $ execStateT (slochDirents children) M.empty
-    unless (M.null lang_to_count_map) $
+slochHierarchy' :: Consumer' DirectoryTree (StateT SlochHierarchy IO) ()
+slochHierarchy' = do
+    (path, children) <- await
+    lang_to_count_map <-
+        liftIO $
+            execStateEffect M.empty $
+                hoist lift (each children) >-> direntToFilePath >-> sloch
+
+    lift $ unless (M.null lang_to_count_map) $
         modify $ M.insert path lang_to_count_map
 
--- | "Inner" lines count, which recursively builds up a language -> line count map from all entries in the specified
--- directory tree.
-slochDirents :: [Dirent] -> StateT (Map Language Int) IO ()
-slochDirents = mapM_ slochDirent
+direntToFilePath :: Monad m => Pipe Dirent FilePath m ()
+direntToFilePath = forever $
+    await >>=
+        \case
+            (DirentDir (_, children)) -> each children >-> direntToFilePath
+            (DirentFile path)         -> yield path
 
--- | "Inner" lines count, as above.
-slochDirent :: Dirent -> StateT (Map Language Int) IO ()
-slochDirent (DirentDir (_, children)) = slochDirents children
-slochDirent (DirentFile file_path) =
+-- Source-lines-of-code from a Producer input.
+slochFiles :: Producer' FilePath IO () -> IO Sloch
+slochFiles files = execStateEffect M.empty $ hoist lift files >-> sloch
+
+-- Generic source-lines-of-code counter that consumes FilePaths in the Sloch state monad.
+sloch :: Consumer' FilePath (StateT Sloch IO) ()
+sloch = forever $ do
+    file_path <- await
     whenMaybe (language file_path) $ \lang -> do
-        n <- lift $ countLines file_path lang
-        modify $ M.insertWith (+) lang n
+        n <- liftIO $ countLines file_path lang
+        lift $ modify $ M.insertWith (+) lang n
 
 -- | Like when, but conditional on a Maybe being Just rather than a Bool being True.
 whenMaybe :: Monad m => Maybe a -> (a -> m ()) -> m ()
